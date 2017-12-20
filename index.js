01941787898f2c96f7a618449b7ce59641faebc9
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2017-12-20 22:29:17 +0200

    feat: find node in tree in both recurive and iterative ways

---
+++ index.js
@@ -0,0 +1,3 @@
+let name = "name";
+
+console.log(name);
---
+++ src/tree-search/tree-search.js
@@ -0,0 +1,32 @@
+//TODO: Optimisation needed here
+const findNodeInTree = (tree, matchingTitle) => {
+  let node;
+  tree.forEach(element => {
+    if (element.title === matchingTitle) {
+      node = element;
+    } else if (element.children) {
+      node = findNodeInTree(element.children, matchingTitle);
+    }
+  });
+  return node;
+}
+
+const findNodeInTreeIterative = (tree, matchingTitle) => {
+  let stack = [...tree],
+    node;
+
+  while (stack.length > 0) {
+    node = stack.pop();
+    if (node.title === matchingTitle) return node;
+    else if (node.children) {
+      node.children.forEach(element => {
+        stack.push(element);
+      });
+    }
+  }
+}
+
+module.exports = {
+  findNodeInTree: findNodeInTree,
+  findNodeInTreeIterative: findNodeInTreeIterative
+};
